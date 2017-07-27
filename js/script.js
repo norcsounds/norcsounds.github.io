63f4e58b20a5e1f49a0c06f85bc7f438ab50266c
Eni Asebiomo <eniolufe@stanford.edu>
2017-07-27 02:22:09 -0700

    Preliminary Setup

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function play(id) {
+	var audio = document.getElementById(id);
+	audio.play();
+}
+
+function tai1() {
+	var audio = document.getElementById('tai1');
+	audio.play();	
+}
+
+function duncan1() {
+	var audio = document.getElementById('duncan1');
+	audio.play();	
+}
+
+function beattie1() {
+	var audio = document.getElementById('beattie1');
+	audio.play();	
+}
+
+function thomas1() {
+	var audio = document.getElementById('thomas1');
+	audio.play();	
+}
+
+function madison1() {
+	var audio = document.getElementById('madison1');
+	audio.play();	
+}
