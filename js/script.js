ddd6d0e4e11f884bda4bdaaa38359de5541c4244
Eni Asebiomo <eniolufe@stanford.edu>
2017-07-28 22:29:34 -0400

    Adds all names

--- js/script.js
+++ js/script.js
@@ -1,29 +1,144 @@
-function play(id) {
-	var audio = document.getElementById(id);
-	audio.play();
+function Alyssa1() {
+	document.getElementById('Alyssa1').play();
 }
 
-function tai1() {
-	var audio = document.getElementById('tai1');
-	audio.play();	
+function Alyssa2() {
+	document.getElementById('Alyssa2').play();
 }
 
-function duncan1() {
-	var audio = document.getElementById('duncan1');
-	audio.play();	
+function Beattie1() {
+	document.getElementById('Beattie1').play();
 }
 
-function beattie1() {
-	var audio = document.getElementById('beattie1');
-	audio.play();	
+function Chase1() {
+	document.getElementById('Chase1').play();
 }
 
-function thomas1() {
-	var audio = document.getElementById('thomas1');
-	audio.play();	
+function Connelly1() {
+	document.getElementById('Connelly1').play();
 }
 
-function madison1() {
-	var audio = document.getElementById('madison1');
-	audio.play();	
+function Connelly2() {
+	document.getElementById('Connelly2').play();
 }
+
+function Dixie1() {
+	document.getElementById('Dixie1').play();
+}
+
+function Duncan1() {
+	document.getElementById('Duncan1').play();
+}
+
+function Duncan2() {
+	document.getElementById('Duncan2').play();
+}
+
+function Duncan3() {
+	document.getElementById('Duncan3').play();
+}
+
+function Eni1() {
+	document.getElementById('Eni1').play();
+}
+
+function Fisayo1() {
+	document.getElementById('Fisayo1').play();
+}
+
+function Fisayo2() {
+	document.getElementById('Fisayo2').play();
+}
+
+function Fisayo3() {
+	document.getElementById('Fisayo3').play();
+}
+
+function Gian1() {
+	document.getElementById('Gian1').play();
+}
+
+function Haley1() {
+	document.getElementById('Haley1').play();
+}
+
+function Keanu1() {
+	document.getElementById('Keanu1').play();
+}
+
+function Kevin1() {
+	document.getElementById('Kevin1').play();
+}
+
+function Kevin2() {
+	document.getElementById('Kevin2').play();
+}
+
+function Madison1() {
+	document.getElementById('Madison1').play();
+}
+
+function Meg1() {
+	document.getElementById('Meg1').play();
+}
+
+function Michael1() {
+	document.getElementById('Michael1').play();
+}
+
+function Michael2() {
+	document.getElementById('Michael2').play();
+}
+
+function Michael3() {
+	document.getElementById('Michael3').play();
+}
+
+function Michael4() {
+	document.getElementById('Michael4').play();
+}
+
+function Priya1() {
+	document.getElementById('Priya1').play();
+}
+
+function Priya2() {
+	document.getElementById('Priya2').play();
+}
+
+function Priya3() {
+	document.getElementById('Priya3').play();
+}
+
+function Randy1() {
+	document.getElementById('Randy1').play();
+}
+
+function Randy2() {
+	document.getElementById('Randy2').play();
+}
+
+function Randy3() {
+	document.getElementById('Randy3').play();
+}
+
+function Tai1() {
+	document.getElementById('Tai1').play();
+}
+
+function Tai2() {
+	document.getElementById('Tai2').play();
+}
+
+function Tai3() {
+	document.getElementById('Tai3').play();
+}
+
+function Tessa1() {
+	document.getElementById('Tessa1').play();
+}
+
+function Thomas1() {
+	document.getElementById('Thomas1').play();
+}
+
